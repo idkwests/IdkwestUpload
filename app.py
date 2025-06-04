fcda8a3c865f8876b9124dad756d1f9fa521898d
Idkwest <nikachkhartishvili32@gmail.com>
2025-06-04 21:48:58 +0400

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,9 @@ def rate(file_id):
 def inject_user():
     return dict(logged_in=('user_id' in session))
 
+# //// Run ////
 if __name__ == '__main__':
-    db.init_db()
-    app.run(debug=True)
+    init_db()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
